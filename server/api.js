38e81e85e80f4cf395d658ae56129d8e221b7b87
Eddie Wang <eddie@eddiewang.me>
2016-05-16 23:04:15 -0400

    fixed error in api route handling for index.html

--- server/api.js
+++ server/api.js
@@ -1,11 +1,7 @@
+import path from 'path';
+
 export default (app) => {
   app.get('/api-test', (req, res) => res.json({
     success: "This page is working"
   }));
-
-
-  // default 404
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/index.html'));
-  });
 }
