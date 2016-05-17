70c23f806bb183662fd4d74d179006a2f98b3ff5
Eddie Wang <eddie@eddiewang.me>
2016-05-16 23:38:27 -0400

    added redux and devtools

--- client/index.js
+++ client/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { hashHistory } from 'react-router';
 import { Provider } from 'react-redux'
 import getRoutes from './config/routes';
+import configureStore from './store/configureStore';
 
-const store = {};
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ client/store/configureStore.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
+
+const finalCreateStore = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+
+export default function configureStore(initialState) {
+  return finalCreateStore(rootReducer, initialState);
+};
